2577c0389774a1d45d2fe2164c81136de2512005
surevishnu1 <vishnunaidusure143@gmail.com>
2024-09-17 11:16:53 +0530

    LED-BLINKING

---
+++ 3.LED-Blinking/led/led_blinking1 (1).ino
@@ -0,0 +1,18 @@
+//
+void detup()
+{
+  pinMode(11,OUTPUT);
+  pinMode(12,OUTPUT;
+ }
+ 
+          void loop()
+          {
+            digitalWrite(11,LOW);
+  digitalWrite(12,HIGH);
+  delay(1000);
+  digitalWrite(11,HIGH);
+  digitalWrite(12,LOW);
+  delay(1000);
+          }
+            
+       
